bab474f3a748d7f67c8dca06e29fa4e67d45526b
Dimas Saifullah Ramadhan <37479804+dimasRamadhan2398@users.noreply.github.com>
2024-09-27 11:31:43 +0700

    Create text_generator.py

---
+++ text_generator.py
@@ -0,0 +1,16 @@
+def text_generator(text):
+  for char in text:
+    yield ord(char)
+
+my_text = st.text_input(
+  "Masukkan text dibawah",
+  placeholder="Masukkan text disini")
+my_generator = text_generator(my_text)
+
+total_char = []
+
+for char in my_generator:
+  total_char.append(char)
+
+char_gabung = ''.join(str(x) for x in total_char)
+print(char_gabung)
